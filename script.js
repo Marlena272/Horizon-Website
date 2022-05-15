c9a05e4cf7688111da2664b8e3d7679afeb281c3
Marlena <marlena272@gmail.com>
2022-05-15 21:11:52 +0200

    Nav + Slider

---
+++ script.js
@@ -0,0 +1,13 @@
+const images = ['graphics/horizon_1.jpg', 'graphics/horizon_2.jpg', 'graphics/horizon_3.jpg', 'graphics/horizon_4.jpg', 'graphics/horizon_5.jpg', 'graphics/horizon_6.jpg', 'graphics/horizon_7.jpg'];
+const sliderImg = document.querySelector('.sliderImg');
+let activeElement = 0;
+
+function changeElement() {
+    activeElement++;
+    if (activeElement == images.length)
+        activeElement = 0;
+    sliderImg.src = images[activeElement]
+
+}
+
+setInterval(changeElement, 5000)
