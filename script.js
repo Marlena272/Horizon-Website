ebf4e1ed558cca236c26bd24e87abd8876adc158
Marlena <marlena272@gmail.com>
2022-06-01 20:07:43 +0200

    Polishing

--- script.js
+++ script.js
@@ -13,6 +13,25 @@ $(document).on('scroll', function () {
         $("nav").css("opacity", "60%");
     }
 
+    let sliderHeight = $(".slider").offset().top;
+    let AboutTheGameHeight = $("#AboutTheGame").offset().top;
+    let AloyHeight = $("#Aloy").offset().top;
+    let HorizonZeroDawnHeight = $("#HorizonZeroDawn").offset().top;
+
+    if ($(window).scrollTop() > sliderHeight) {
+        $(".aboutTheGameText").addClass("opacity");
+    }
+    if ($(window).scrollTop() > AboutTheGameHeight) {
+        $(".Aloy_Text").addClass("opacity");
+    }
+    if ($(window).scrollTop() > AloyHeight) {
+        $(".horizonZeroDawnText").addClass("opacity");
+    }
+    if ($(window).scrollTop() > HorizonZeroDawnHeight) {
+        $(".aboutTheGameTextForbidden").addClass("opacity");
+        console.log("Heja3");
+    }
+
 })
 
 $('.navigationList a').on("click", function () {
